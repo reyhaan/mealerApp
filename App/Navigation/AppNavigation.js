d5ec7a56b17a246a8e93a3a3bfb3e7bb9eef612d
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2017-10-29 19:06:49 -0400

    refactor tab navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -3,44 +3,9 @@ import OrdersTab from '../Containers/OrdersTab'
 import SettingsTab from '../Containers/SettingsTab'
 import CooksTab from '../Containers/CooksTab'
 import { AuthScreen, SignUpScreen } from '../Containers'
+import TabsView from './AppTabNavigation'
 
 import styles from './Styles/NavigationStyles'
-
-const tabNavigatorConfig = {
-  swipeEnabled: false,
-  tabBarPosition: 'bottom',
-  headerMode: 'none',
-  navigationOptions: {
-    headerStyle: styles.header
-  }
-}
-
-const tabsList = {
-  One: {
-      screen:  CooksTab,
-      headerMode: 'none',
-      navigationOptions: {
-          title: 'Cooks',
-          headerStyle: styles.header
-      },
-  },
-
-  Two: {
-      screen: OrdersTab,
-      navigationOptions: {
-          title: 'Orders',
-      },
-  },
-
-  Three: {
-      screen: SettingsTab,
-      navigationOptions: {
-          title: 'Settings',
-      },
-  }
-}
-
-const TabsView = TabNavigator(tabsList, tabNavigatorConfig)
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({

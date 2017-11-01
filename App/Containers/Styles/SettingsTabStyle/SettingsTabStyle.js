788870d93cddafb5c81111c228b0cc3a66f9107b
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2017-11-01 01:20:12 -0400

    add menu items in settings tab

--- App/Containers/Styles/SettingsTabStyle/SettingsTabStyle.js
+++ App/Containers/Styles/SettingsTabStyle/SettingsTabStyle.js
@@ -5,5 +5,22 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background
+  },
+  listContainer: {
+    borderBottomWidth: 0,
+    borderTopWidth: 0, 
+    marginBottom: 20, 
+    paddingLeft: 5, 
+    paddingRight: 5, 
+    backgroundColor: Colors.background
+  },
+  listWrapper: {
+    borderTopColor: Colors.background
+  },
+  listTitle: {
+    color: Colors.snow
+  },
+  listItem: {
+    borderBottomColor: Colors.pink2
   }
 })

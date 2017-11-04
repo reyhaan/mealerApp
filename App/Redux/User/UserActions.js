2b4be8f2f5b9df4ce55496788ffe68448771ab91
tktaofik <tktaofik@users.noreply.github.com>
2017-11-04 13:30:48 -0400

    Clean up to the redux

--- App/Redux/User/UserActions.js
+++ App/Redux/User/UserActions.js
@@ -1,10 +1,4 @@
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_UP = 'SIGN_UP';
-export const RESET = 'RESET';
-
-export const signIn = (data) => action(SIGN_IN, data);
-export const signUp = (data) => action(SIGN_UP, data);
-
-function action(type, data) {
-    return {type, data};
-}
+export default  {
+    signIn: 'SIGN_IN',
+    signUp: 'SIGN_UP'
+};

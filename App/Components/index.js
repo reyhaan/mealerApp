6b409f19dece2fc753b1620e8022f605c472bf2a
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2017-11-11 21:03:07 -0500

    add functionality for creating a new menu item

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,7 +1,9 @@
 import LoadingSpinner from './LoadingSpinner'
 import Header from './Header'
+import MlImagePicker from './MlImagePicker'
 
 export {
     Header,
-    LoadingSpinner
+    LoadingSpinner,
+    MlImagePicker
 };

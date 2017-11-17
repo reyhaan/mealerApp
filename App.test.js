15f001c22a7395b3626adc90619d2f08e7420c78
tktaofik <tktaofik@users.noreply.github.com>
2017-11-16 22:48:09 -0500

    progress with tests

---
+++ App.test.js
@@ -0,0 +1,15 @@
+import 'react-native';
+import React from 'react';
+import App from './App';
+import renderer from 'react-test-renderer';
+
+it('renders the loading screen', async () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
+it('works', () => {
+    expect(1).toBe(1);
+});
+

24c2e47e0f293dd0f53c6a02b7f337e5204b9d92
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2017-11-19 19:31:45 -0500

    reactotron changes to store file

--- App/Redux/createStore.js
+++ App/Redux/createStore.js
@@ -1,14 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import {rootReducer} from '../Redux/rootReducer'
 import rootSaga from './rootSaga'
-import Reactotron from 'reactotron-react-native'
 
 export default function configureStore() {
-
-    const sagaMonitor = Reactotron.createSagaMonitor()
-    const sagaMiddleware = createSagaMiddleware()
-
+    const sagaMiddleware = createSagaMiddleware();
     const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
     sagaMiddleware.run(rootSaga);
     return store;

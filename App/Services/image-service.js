afa8fec874df130bd72c879c3155d96e112cea6b
tktaofik <tktaofik@users.noreply.github.com>
2017-12-07 23:44:23 -0500

    Cloud function now used for img uplaod

--- App/Services/image-service.js
+++ App/Services/image-service.js
@@ -5,15 +5,15 @@ const img = {};
 // storage reference
 img.uploadBase64Image = async (name, base64Img) => {
     try {
-        name = name.replace(/\s/g, '')  + "mealer_app_image"; // remove spaces from name
+        name = name.replace(/\s/g, '') + "mealer_app_image"; // remove spaces from name
         const body = {name, base64Img};
-        const SERVER_URL = 'https://airnd-rest-api-dev.herokuapp.com/';
+        const SERVER_URL = 'https://us-central1-mealer-app.cloudfunctions.net/addMessage';
+        //const LOCAL_URL = "http://localhost:5000/mealer-app/us-central1/addMessage";
         const response = await axios.post(SERVER_URL + "api/image/uploadImage", body);
         return response.data.imageURL
     } catch (e) {
         return e
     }
 };
-
 
 export default img;

62dd89e76c678b848f6ba2a039517a4b404f7b17
tktaofik <tktaofik@users.noreply.github.com>
2017-12-10 07:57:57 -0500

    Added functions properly

---
+++ cloud_functions/functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+const calculateQuota = () => {
+    return new Promise((resolve) => {
+        resolve(null)
+    })
+};
+
+exports.merchantQuotaUpdate = functions.database.ref('/orders/{orderId}')
+    .onWrite(event => {
+        console.log(event.params);
+        console.log(event.data.val());
+        console.log("test");
+
+        return calculateQuota;
+    });

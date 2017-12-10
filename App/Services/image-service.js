6f86f860f3e74750c5d5b98bfb03827da582d4c6
tktaofik <tktaofik@users.noreply.github.com>
2017-12-10 07:35:14 -0500

    clean up

--- App/Services/image-service.js
+++ App/Services/image-service.js
@@ -6,12 +6,10 @@ const img = {};
 // storage reference
 img.uploadBase64Image = async (name, base64Img) => {
     try {
-        // const SERVER_URL = 'https://us-central1-mealer-app.cloudfunctions.net/uploadBase64ImageToS3';
+        const SERVER_URL = 'https://airnd-rest-api-dev.herokuapp.com/api/image/uploadImage';
         // const LOCAL_URL = "http://localhost:5000/mealer-app/us-central1/uploadBase64ImageToS3";
-
         name = name.replace(/\s/g, '') + "mealer_app_image"; // remove spaces from name
         const body = {name, base64Img};
-        const SERVER_URL = 'https://airnd-rest-api-dev.herokuapp.com/api/image/uploadImage';
         const response = await axios.post(SERVER_URL, body);
 
         return response.data.imageURL

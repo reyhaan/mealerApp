9bad6f4a916317aacac1b2fbe4b0af51db54dfc8
Mohammad Rehaan <rehaan@Mohammads-iMac.local>
2017-12-24 03:22:20 -0500

    blehhh

--- App/Components/Avatar.js
+++ App/Components/Avatar.js
@@ -13,11 +13,11 @@ class Avatar extends Component {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 10
+        marginTop: 20
         }}
       >
         <Image source={image} style={{width: 150, height: 150, borderRadius:72 }}/>
-        <View style={{ padding: 10, flex: 1, flexDirection: "row", alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+        <View style={{ marginTop: 15, padding: 10, flex: 1, flexDirection: "row", alignItems: 'flex-start', justifyContent: 'flex-start' }}>
           <Icon size={20} name='upload' color={Colors.background} type='font-awesome'/>
           <Text style={{ marginLeft: 10, paddingTop: 2, color: Colors.background, fontWeight: "bold"}}>UPLOAD</Text>
         </View>

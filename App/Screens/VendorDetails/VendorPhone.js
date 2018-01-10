281e4ca0ff01ba6fa767fceeadc391d10d36cdd1
uk1288 <ukbassey93@gmail.com>
2018-01-09 21:15:53 -0500

    added call, email vendor and use map to open vendor address

--- App/Screens/VendorDetails/VendorPhone.js
+++ App/Screens/VendorDetails/VendorPhone.js
@@ -17,46 +17,46 @@ export default class VendorPhone extends Component {
   }
     render() {
         if (this.props.phone) {
-            return (
-                <View>
-                    <Row style={{
-                        borderBottomColor: Colors.steel,
-                        borderBottomWidth: 1,
-                        paddingBottom: 10,
-                        marginBottom: 10
-                    }}>
-                        <Text style={{color: Colors.gray, fontWeight: 'bold'}}>PHONE</Text>
-                    </Row>
-                    <TouchableOpacity onPress={this._callVendor}>
-                      <Row style={{marginBottom: 20}}>
-                          <Text>{this.props.phone}</Text>
-                          <View
-                            style = {{
-                              position: 'absolute',
-                              right: 30,
-                              display: 'flex',
-                              flexDirection: 'row',
-                              justifyContent: 'flex-end'
-                            }}>
-                            <View
-                              style = {{
-                                height: 30,
-                                width: 1,
-                                backgroundColor: Colors.steel
-                              }}
-                            />
-                            <Icon
-                              raised
-                              name='phone-square'
-                              type='font-awesome'
-                              color='#87CEFA'
-                              size={13}
-                              containerStyle	={{marginTop: 0, marginLeft: 20}} />
-                          </View>
-                      </Row>
-                    </TouchableOpacity>
-                </View>
-            )
+          return (
+            <View>
+              <Row style={{
+                  borderBottomColor: Colors.steel,
+                  borderBottomWidth: 1,
+                  paddingBottom: 10,
+                  marginBottom: 10
+              }}>
+                <Text style={{color: Colors.gray, fontWeight: 'bold'}}>PHONE</Text>
+              </Row>
+              <TouchableOpacity onPress={this._callVendor}>
+                <Row style={{marginBottom: 20}}>
+                  <Text>{this.props.phone}</Text>
+                  <View
+                    style = {{
+                      position: 'absolute',
+                      right: 30,
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'flex-end'}}>
+                    <View
+                      style = {{
+                        height: 30,
+                        width: 1,
+                        backgroundColor: Colors.steel
+                      }}
+                    />
+                    <Icon
+                      raised
+                      name='phone-square'
+                      type='font-awesome'
+                      color='#87CEFA'
+                      size={13}
+                      containerStyle	={{marginTop: 0, marginLeft: 20}}
+                    />
+                  </View>
+                  </Row>
+                </TouchableOpacity>
+            </View>
+          )
         } else {
             return null
         }
